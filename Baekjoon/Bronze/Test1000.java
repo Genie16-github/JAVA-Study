bdfee528b67525b32a54e3d5c620e6473d026e4e
JaeBeom <4411dlwoqja@naver.com>
2022-12-01 14:07:22 +0900

    commit

--- Baekjoon/Bronze/Test1000.java
+++ Baekjoon/Bronze/Test1000.java
@@ -1,5 +1,12 @@
-public class Test1000{
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+import java.io.IOException;
+
+public class Test1000 {
+    public static void main(String[] arg) throws IOException {
+            int A = System.in.read() - '0';
+            System.in.read();
+            int B = System.in.read() - '0';
+            System.out.println(A+B);
+
+
+    }
 }
