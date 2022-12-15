c3781f84f64736e8bb4dbc7d8de4c0b0ec42b60c
JaeBeom <4411dlwoqja@naver.com>
2022-12-15 15:32:32 +0900

    commit

---
+++ Baekjoon/Silver/Test1026.java
@@ -0,0 +1,38 @@
+// [S4]보물
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Test1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> A = new ArrayList<>();
+        ArrayList<Integer> B = new ArrayList<>();
+
+        int res = 0;
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());      
+        for (int i = 0; i < n; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        Collections.sort(A);
+        Collections.sort(B, Collections.reverseOrder());
+        
+        for (int i = 0; i < n; i++) {
+            res += A.get(i) * B.get(i);
+        }
+        
+        System.out.println(res);
+    }
+}
