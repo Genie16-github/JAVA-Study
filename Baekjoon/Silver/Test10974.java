9d1a8de10ba0c34d7d9bd1ceddb917342050b273
JaeBeom <4411dlwoqja@naver.com>
2022-12-23 20:35:30 +0900

    commit

---
+++ Baekjoon/Silver/Test10974.java
@@ -0,0 +1,33 @@
+// [S3]모든 순열
+import java.util.*;
+
+public class Test10974{
+	static int n, arr[], isSelected[];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		arr = new int[n];
+		isSelected = new int[n];
+		
+		dfs(0);
+	}
+	
+	static void dfs(int v) {
+		if(v == n) {
+			for(int i=0; i<n; i++) System.out.print(arr[i] + " ");
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0 ; i<n; i++) {
+			if(isSelected[i]==1)
+				continue;
+			arr[v] = i+1;
+			isSelected[i] = 1;
+			dfs(v+1);
+			isSelected[i] = 0;
+		}
+	}
+}
