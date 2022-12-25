9e6070e96586d6f4f1b6d0ea8dc4313f5773ca94
JaeBeom <4411dlwoqja@naver.com>
2022-12-25 17:44:26 +0900

    commit

---
+++ Baekjoon/Silver/Test2012.java
@@ -0,0 +1,26 @@
+// [S3]등수 매기기
+import java.io.IOException;
+import java.util.*;
+
+public class Test2012{
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] p_rank = new int[n+1];
+		
+		for(int i=1; i<n+1; i++) {
+			p_rank[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(p_rank);
+		
+		long res = 0;
+		for(int i=1; i<p_rank.length;i++) {
+			res += Math.abs(i-p_rank[i]);
+		}
+		
+		System.out.println(res);
+	}
+}
