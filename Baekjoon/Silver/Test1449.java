28852db58d2b5fe4299e055e439c22e43a955332
JaeBeom <4411dlwoqja@naver.com>
2022-12-26 11:25:57 +0900

    commit

---
+++ Baekjoon/Silver/Test1449.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// [S3]¼ö¸®°ø Ç×½Â
+public class Test1449 {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+		
+		int[] x = new int[N];
+		for(int i=0; i<N; i++) {
+			x[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(x);
+		
+		double position = x[0] - 0.5;
+		int cnt = 1;
+		for(int i=0; i<N ; i++) {
+			if(position+L < x[i]) {
+				cnt++;
+				position = x[i] - 0.5;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
