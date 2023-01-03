f34783bde12f709323bd87b469f23d7a47c25af0
JaeBeom <4411dlwoqja@naver.com>
2023-01-03 20:47:52 +0900

    commit

---
+++ Baekjoon/Silver/Test1254.java
@@ -0,0 +1,34 @@
+// [S2]팰린드롬 만들기
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test1254 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s = in.readLine();
+        int res = s.length();
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(isPalind(s.substring(i))){
+                break;
+            }
+            res++;
+        }
+        System.out.println(res);
+    }
+
+    private static boolean isPalind(String s) {
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            if (s.charAt(start) != s.charAt(last))
+                return false;
+            start++;
+            last--;
+        }
+        return true;
+    }
+}
